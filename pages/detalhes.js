0e740cbc56669e19afed22863f117755d2a91e7e
nychavs <chavesnaiury@gmail.com>
2023-05-12 11:29:49 -0300

    added description

--- pages/detalhes.js
+++ pages/detalhes.js
@@ -1,18 +1,47 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image} from "react-native";
 import { TouchableOpacity } from "react-native";
+import { EvilIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 const Detalhes = ({navigation, route}) =>{
-    const { id, nome} = route.params;
+    const { id, nome, autor, valor, numPages, descricao} = route.params;
     return(
-        <View className="top-8">
-            <TouchableOpacity
-            onPress={()=>{
-                navigation.navigate("Home")}}>
-                <Text>VOLTAR</Text>
-            </TouchableOpacity>
-                <Text> Nome: {nome} </Text>
+        <View className="flex h-screen relative">
+        <View className="absolute h-20 w-full bg-navbar opacity-opa-navbar">
+         <View className="top-8 left-[81%]">
+        <EvilIcons name="cart" size={46} color="white"/>
+         </View>
+         <View className="left-8">
+            <MaterialCommunityIcons name="keyboard-backspace"
+            size={32} color="white"
+            onPress={()=>{navigation.navigate("Home")}}/>
         </View>
+            <View className="top-8  self-center">
+                <Text className="text-xl self-center">More information</Text>
+                    <View className="bg-btn mt-2 w-80 h-0.5"></View>
+                    <View className="flex-row justify-between top-5">
+                        <Image source={require('C:/Users/46404521873/Desktop/booklivery/assets/daisy.jpg')} 
+                        style={{width:130, height:200, borderRadius:9}}
+                        />
+                        <View className="flex-column w-44 justify-evenly">
+                            <Text className="text-lg">{nome}</Text>
+                            <Text className="text-lg">Autor: {autor}</Text>
+                            <Text className="text-lg">{numPages} pages </Text>
+                            <Text className="text-lg bg-btn-clear h-8 w-20 text-center rounded">$ {valor}</Text>
+                        </View>
+                        </View>
+                        <View className="mt-12 w-80 h-40 self-center">
+                            <Text className="text-base">{descricao}</Text>
+                            <TouchableOpacity>
+                                <Text className="text-navbar"> + See more</Text>
+                            </TouchableOpacity>
+                        </View>
+            </View>
+        </View>
+        </View>
+     
     )
 }
 
